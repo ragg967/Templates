486ad1ca9a4f31b31dfb5c7eaa23aded53b82af1
Ragg967 <rywatson1027@gmail.com>
2025-12-16 17:20:55 -0800

    yay

---
+++ flake/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Project development environment/Project name";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # packages here remember to run 'direnv allow' to make it work
+        ];
+
+        shellHook = ''
+          export PROJECT_ROOT=$(pwd)
+          echo "Development environment loaded!"
+        '';
+      };
+    };
+}
